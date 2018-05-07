61557cb79a050d727c6beabddf9e3af9f3105c43
XelMed <Xelmed91@gmail.com>
2018-05-07 16:11:08 +0800

    add an opportunity to choose file type and name with attributes outType and outName

--- test/fopTest.js
+++ test/fopTest.js
@@ -14,17 +14,47 @@ describe('fop pdf', function () {
   })
 
   it('should not fail when rendering', function () {
-    this.timeout(10000);
+    this.timeout(10000)
     var request = {
       template: { content: fs.readFileSync(path.join(__dirname, '/test.xsl')),
         recipe: 'fop-xsl-pdf',
         engine: 'none' },
-      data:fs.readFileSync(path.join(__dirname, '/test.json'))
-
+      data: {data: '<name>Frank</name>'}
     }
 
     return reporter.render(request, {}).then(function (response) {
       response.content.toString().should.containEql('%PDF')
+    })
+  })
+  it('should return rtf', function () {
+    this.timeout(10000)
+    var request = {
+      template: { content: fs.readFileSync(path.join(__dirname, '/test.xsl')),
+        recipe: 'fop-xsl-pdf',
+        engine: 'none' },
+      data: {
+        data: '<name>Frank</name>',
+        outType: 'rtf'}
+    }
+
+    return reporter.render(request, {}).then(function (response) {
+      response.headers['Content-Disposition'].toString().should.containEql('rtf')
+    })
+  })
+  it('should return rtf', function () {
+    this.timeout(10000)
+    var request = {
+      template: { content: fs.readFileSync(path.join(__dirname, '/test.xsl')),
+        recipe: 'fop-xsl-pdf',
+        engine: 'none' },
+      data: {
+        data: '<name>Frank</name>',
+        outName: 'TestName_NameTest',
+        outType: 'rtf'}
+    }
+
+    return reporter.render(request, {}).then(function (response) {
+      response.headers['Content-Disposition'].toString().should.containEql('TestName_NameTest')
     })
   })
 })
